a8cfdd9167801fa56aff927786e80f9a4b84f09d
gitu1997 <gitu_poke@yahoo.com>
2017-01-29 01:23:33 +0530

    All source code and data added

---
+++ CMakeFiles/PathPlanning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PathPlanning.dir/src/PathPlanning.o"
+  "PathPlanning.pdb"
+  "PathPlanning"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PathPlanning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
